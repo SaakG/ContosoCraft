26bfa27bc40779d7ea1a133d4f509564b3bf367e
Shaharyar.Ayaz <Shaharyar.Ayaz@SHAHARYAR1>
2023-01-21 11:38:06 +0500

    Add project files.

---
+++ ContosoCraft.WebSite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContosoCraft.WebSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
